64ad6831146eae856555a5448ca6a9ae8854b2e6
Simo672k <mohammed.hakmiii555@gmail.com>
2023-06-27 11:58:14 +0200

    tabs colors fixed

--- src/components/TabHead.jsx
+++ src/components/TabHead.jsx
@@ -5,11 +5,6 @@ function TabHead({iconImg, descrp, actif}) {
     <button className={"tab tab-lifted py-2"+(actif? " tab-active active-tab" : " border-0 h-fit")} style={{height: "fit-content !important"}}>
       <Icon iconImg={iconImg}/>
       <span className="text-lg text-gray-600 font-semibold">{descrp}</span>
-      {
-        actif ? 
-        <span className="hide-botto-border"></span> :
-        null
-      }
     </button>
   )
 }
--- src/components/TabsHeaderContainer.jsx
+++ src/components/TabsHeaderContainer.jsx
@@ -1,6 +1,6 @@
 function TabsHeaderContainer({children}) {
   return (
-    <div className="bg-white pt-8 flex justify-center border-b">
+    <div className="bg-white pt-8 flex justify-center">
       <div className="tabs">{children}</div>
     </div>
   )
